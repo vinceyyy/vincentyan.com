ea8630a0cc4c4655f6370421e6eaf1ecd833c9a6
Vincent Yan <vincenty17@icloud.com>
2022-01-14 17:48:50 -0800

    fix: Safari complains about using rem for svg size

--- src/components/Social.svelte
+++ src/components/Social.svelte
@@ -4,9 +4,9 @@
 
 <div class="flex items-center justify-between my-4">
     <a class="px-4 duration-150" href="https://github.com/vinceyyy">
-        <SocialIncon name="github" width="3rem" height="3rem" />
+        <SocialIncon name="github" width="2em" height="2em" />
     </a>
     <a class="px-4 duration-150" href="https://www.linkedin.com/in/vincentwyan/"
-        ><SocialIncon name="linkedin" width="3rem" height="3rem" />
+        ><SocialIncon name="linkedin" width="2em" height="2em" />
     </a>
 </div>
