78bd9b65976dbd4828f64c5b90e5cb35a55177f4
Vincent Yan <vincenty17@icloud.com>
2022-01-14 17:29:08 -0800

    feat: replace icon lib with custom component
    No easy way to control color transit with lib, so use raw svg instead.

--- src/components/Social.svelte
+++ src/components/Social.svelte
@@ -1,42 +1,12 @@
 <script lang="ts">
-    import SocialIcons from "@rodneylab/svelte-social-icons";
-    import Hoverable from "./Hoverable.svelte";
+    import SocialIncon from "./SocialIcon.svelte";
 </script>
 
 <div class="flex items-center justify-between my-4">
-    <Hoverable let:hovering={active}>
-        <a class="px-4 duration-150" href="https://github.com/vinceyyy"
-            >{#if active}
-                <SocialIcons
-                    network="github"
-                    fgColor="#eeeeee"
-                    bgColor="#00B0F0"
-                />
-            {:else}<SocialIcons
-                    network="github"
-                    fgColor="#eeeeee"
-                    bgColor="#111111"
-                />
-            {/if}
-        </a>
-    </Hoverable>
-
-    <Hoverable let:hovering={active}>
-        <a
-            class="px-4 duration-150"
-            href="https://www.linkedin.com/in/vincentwyan/"
-            >{#if active}
-                <SocialIcons
-                    network="linkedin"
-                    fgColor="#eeeeee"
-                    bgColor="#00B0F0"
-                />
-            {:else}<SocialIcons
-                    network="linkedin"
-                    fgColor="#eeeeee"
-                    bgColor="#111111"
-                />
-            {/if}
-        </a>
-    </Hoverable>
+    <a class="px-4 duration-150" href="https://github.com/vinceyyy">
+        <SocialIncon name="github" width="3rem" height="3rem" />
+    </a>
+    <a class="px-4 duration-150" href="https://www.linkedin.com/in/vincentwyan/"
+        ><SocialIncon name="linkedin" width="3rem" height="3rem" />
+    </a>
 </div>
